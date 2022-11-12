846b33589fdfebcfd9c1d7b9e890a66676210fe5
SspAppunik <shubham.prajapati@appunik.com>
2022-11-12 18:57:33 +0530

    get api data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './services/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'demo';
+  
+  constructor(private service:PostService) {}
+  user : any = []
+  
+  ngOnInit() {
+    this.service.getPosts()
+      .subscribe(response => {
+        console.log(response);
+         this.user = response.data;
+      });
+}
 }
